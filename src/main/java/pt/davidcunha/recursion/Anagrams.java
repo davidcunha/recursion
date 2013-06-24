918c1f2fc1511a5a7fbe421f5fa6afde8579e4aa
davidcunha <davidgoncalvescunha@gmail.com>
2013-06-24 23:29:11 +0100

    brecursive binary search

--- src/main/java/pt/davidcunha/recursion/Anagrams.java
+++ src/main/java/pt/davidcunha/recursion/Anagrams.java
@@ -9,5 +9,13 @@ package pt.davidcunha.recursion;
  * @author David Cunha <davidgoncalvescunha AT gmail DOT pt>
  */
 public class Anagrams {
-    
+
+    public Anagrams() {
+    }
+
+    public void doAnagram() {
+    }
+
+    public void rotate() {
+    }
 }
---
+++ src/main/java/pt/davidcunha/recursion/BinarySearch.java
@@ -0,0 +1,94 @@
+/*
+ * Binary Search
+ * 
+ */
+package pt.davidcunha.recursion;
+
+/**
+ *
+ * @author David Cunha <davidgoncalvescunha AT gmail DOT pt>
+ */
+public class BinarySearch {
+
+    private int nElemns;
+    private long[] arr;
+
+    /**
+     * BinarySearch public constructor
+     *
+     * @param max array initialization
+     */
+    public BinarySearch(int max) {
+        arr = new long[max];
+        nElemns = 0;
+    }
+
+    /**
+     * BinarySearch getter
+     *
+     * @return array
+     */
+    public long[] getArr() {
+        return arr;
+    }
+
+    /**
+     * Insert function (sorted)
+     *
+     * @param n number to insert in the array
+     */
+    public void insert(long n) {
+        arr[nElemns] = n;
+        nElemns++;
+    }
+
+    /**
+     * Display function
+     *
+     * @return string with all elements
+     */
+    public String display() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < nElemns; i++) {
+            sb.append(arr[i]);
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+
+    /**
+     * Find wrapper for binary search
+     *
+     * @param n element to be searched
+     * @return element
+     */
+    public int find(long n) {
+        return this.binarySearch(n, 0, nElemns - 1);
+    }
+
+    /**
+     * Binary Search
+     *
+     * @param n element to be searched
+     * @return element
+     */
+    public int binarySearch(long n, int lb, int ub) {
+        int mid;
+
+        mid = (lb + ub) / 2;
+        if (arr[mid] == n) {
+            return mid;
+        } else if (lb > ub) {
+            return arr.length;
+        } else {
+            if (n > arr[mid]) {
+                lb = mid + 1;
+                return this.binarySearch(n, lb, ub);
+            } else {
+                ub = mid - 1;
+                return this.binarySearch(n, lb, ub);
+            }
+        }
+
+    }
+}
